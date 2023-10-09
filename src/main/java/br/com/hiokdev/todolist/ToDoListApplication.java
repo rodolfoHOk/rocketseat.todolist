d5308f187b6fcaa079cedf19503cd9c16903668a
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-09 10:28:20 -0300

    refactor: rename application class

--- src/main/java/br/com/hiokdev/todolist/ToDoListApplication.java
+++ src/main/java/br/com/hiokdev/todolist/ToDoListApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodolistApplication {
+public class ToDoListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodolistApplication.class, args);
+		SpringApplication.run(ToDoListApplication.class, args);
 	}
 
 }
