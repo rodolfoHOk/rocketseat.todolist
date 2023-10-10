bcd1516ff364a24efeeb1b275353e1a963a26962
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-10 10:55:31 -0300

    feat: update create user endpoint to persist in database

--- src/main/java/br/com/hiokdev/todolist/user/controller/UserController.java
+++ src/main/java/br/com/hiokdev/todolist/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package br.com.hiokdev.todolist.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,16 +9,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.hiokdev.todolist.user.model.UserModel;
+import br.com.hiokdev.todolist.user.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserController {
+
+  private final IUserRepository userRepository;
   
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
   public UserModel create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getName());
-    return userModel;
+    var userCreated = userRepository.save(userModel);
+    return userCreated;
   }
 
 }
