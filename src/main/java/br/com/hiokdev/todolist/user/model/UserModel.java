6a6fb5c1f57bc0d2d4f1ebd91a243912780df357
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-10 10:46:57 -0300

    feat: create user entity

--- src/main/java/br/com/hiokdev/todolist/user/model/UserModel.java
+++ src/main/java/br/com/hiokdev/todolist/user/model/UserModel.java
@@ -1,16 +1,37 @@
 package br.com.hiokdev.todolist.user.model;
 
-import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@Table(name = "tb_users")
 public class UserModel {
   
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(nullable = false, unique = true, length = 50)
   private String username;
+
+  @Column(nullable = false, length = 150)
   private String name;
+  
+  @Column(nullable = false)
   private String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
 }
