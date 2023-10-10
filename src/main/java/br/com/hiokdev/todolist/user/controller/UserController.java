006ece3a188b325039d310071bd8ab2a4d6b20c8
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-10 11:13:41 -0300

    feat: validate if user exists before persist new user

--- src/main/java/br/com/hiokdev/todolist/user/controller/UserController.java
+++ src/main/java/br/com/hiokdev/todolist/user/controller/UserController.java
@@ -2,10 +2,10 @@ package br.com.hiokdev.todolist.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.hiokdev.todolist.user.model.UserModel;
@@ -20,10 +20,15 @@ public class UserController {
   private final IUserRepository userRepository;
   
   @PostMapping
-  @ResponseStatus(code = HttpStatus.CREATED)
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity<?> create(@RequestBody UserModel userModel) {
+    var existUser = userRepository.findByUsername(userModel.getUsername());
+
+    if (existUser.isPresent()) {
+      return ResponseEntity.badRequest().body("Usuário já existe");
+    }
+
     var userCreated = userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 
 }
--- src/main/java/br/com/hiokdev/todolist/user/repository/IUserRepository.java
+++ src/main/java/br/com/hiokdev/todolist/user/repository/IUserRepository.java
@@ -1,11 +1,15 @@
 package br.com.hiokdev.todolist.user.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.hiokdev.todolist.user.model.UserModel;
 
+
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+  
+  Optional<UserModel> findByUsername(String username);
   
 }
