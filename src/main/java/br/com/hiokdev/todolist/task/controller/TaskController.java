f46c47944c096f996b5978b8c132f34ed5e8a493
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-12 11:29:37 -0300

    feat: validate startAt and endAt dates

--- src/main/java/br/com/hiokdev/todolist/task/controller/TaskController.java
+++ src/main/java/br/com/hiokdev/todolist/task/controller/TaskController.java
@@ -1,5 +1,6 @@
 package br.com.hiokdev.todolist.task.controller;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,16 @@ public class TaskController {
   public ResponseEntity<?> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
     var userId = request.getAttribute("userId");
     taskModel.setUserId((UUID) userId);
+
+    var currentDate = LocalDateTime.now();
+    System.out.println(currentDate.toString());
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      System.out.println(taskModel.getStartAt());
+      return ResponseEntity.badRequest().body("A data de início / término deve ser maior a data atual");
+    }
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.badRequest().body("A data de término deve ser maior a data de início");
+    }
     
     var taskCreated = taskRepository.save(taskModel);
     return ResponseEntity.ok().body(taskCreated);
